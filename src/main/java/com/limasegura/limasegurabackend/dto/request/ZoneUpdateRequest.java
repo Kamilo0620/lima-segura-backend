2384b5f72dfc821dde4c7a44e3451a552061d6c1
Elías <elias.paz@utec.edu.pe>
2026-09-24 21:34:31 -0500

    Añadidos atributos latitude y longitude que antes no estaban pero se usaban en la lógica

--- src/main/java/com/limasegura/limasegurabackend/dto/request/ZoneUpdateRequest.java
+++ src/main/java/com/limasegura/limasegurabackend/dto/request/ZoneUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.limasegura.limasegurabackend.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -15,4 +16,8 @@ public class ZoneUpdateRequest {
     @NotBlank(message = "El distrito es obligatorio")
     @Size(max = 100,message="El nombre del distrito no puede superar los 100 caracteres")
     private String district;
+    @NotNull(message = "La latitud es obligatoria")
+    private Double latitude;
+    @NotNull(message = "La longitud es obligatoria")
+    private Double longitude;
 }
